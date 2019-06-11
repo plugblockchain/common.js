polkadot-js/common

Allow string params for sr25519/ed25519 sign & verify (#404)
* Allow string params for sr25519/ed25519 sign & verify

* Bump wasm

* Remove _'s from params (code-completion)

packages/util-crypto/src/nacl/sign.ts
@@ -5,7 +5,7 @@
 import { Keypair } from '../types';
 
 import nacl from 'tweetnacl';
-import { assert } from '@polkadot/util';
+import { assert, u8aToU8a } from '@polkadot/util';
 import { isReady, ed25519Sign } from '@polkadot/wasm-crypto';
 
 /**
@@ -22,10 +22,12 @@ import { isReady, ed25519Sign } from '@polkadot/wasm-crypto';
  * naclSign([...], [...]); // => [...]
  * ```
  */
-export default function naclSign (message: Uint8Array, { publicKey, secretKey }: Partial<Keypair>): Uint8Array {
+export default function naclSign (message: Uint8Array | string, { publicKey, secretKey }: Partial<Keypair>): Uint8Array {
   assert(secretKey, 'Expected valid secretKey');
 
+  const messageU8a = u8aToU8a(message);
+
   return isReady()
-    ? ed25519Sign(publicKey as Uint8Array, (secretKey as Uint8Array).subarray(0, 32), message)
-    : nacl.sign.detached(message, secretKey as Uint8Array);
+    ? ed25519Sign(publicKey as Uint8Array, (secretKey as Uint8Array).subarray(0, 32), messageU8a)
+    : nacl.sign.detached(messageU8a, secretKey as Uint8Array);
 }

packages/util-crypto/src/nacl/verify.ts
@@ -3,6 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import nacl from 'tweetnacl';
+import { u8aToU8a } from '@polkadot/util';
 import { isReady, ed25519Verify } from '@polkadot/wasm-crypto';
 
 /**
@@ -19,8 +20,12 @@ import { isReady, ed25519Verify } from '@polkadot/wasm-crypto';
  * naclVerify([...], [...], [...]); // => true/false
  * ```
  */
-export default function naclVerify (message: Uint8Array, signature: Uint8Array, publicKey: Uint8Array): boolean {
+export default function naclVerify (message: Uint8Array | string, signature: Uint8Array | string, publicKey: Uint8Array | string): boolean {
+  const messageU8a = u8aToU8a(message);
+  const signatureU8a = u8aToU8a(signature);
+  const publicKeyU8a = u8aToU8a(publicKey);
+
   return isReady()
-    ? ed25519Verify(signature, message, publicKey)
-    : nacl.sign.detached.verify(message, signature, publicKey);
+    ? ed25519Verify(signatureU8a, messageU8a, publicKeyU8a)
+    : nacl.sign.detached.verify(messageU8a, signatureU8a, publicKeyU8a);
 }

packages/util-crypto/src/schnorrkel/sign.ts
@@ -6,16 +6,18 @@ import { Keypair } from '../types';
 
 import '../polyfill';
 
-import { assert } from '@polkadot/util';
+import { assert, u8aToU8a } from '@polkadot/util';
 import { sr25519Sign } from '@polkadot/wasm-crypto';
 
 /**
  * @name schnorrkelSign
  * @description Returns message signature of `message`, using the supplied pair
  */
-export default function schnorrkelSign (message: Uint8Array, { publicKey, secretKey }: Partial<Keypair>): Uint8Array {
+export default function schnorrkelSign (message: Uint8Array | string, { publicKey, secretKey }: Partial<Keypair>): Uint8Array {
   assert(publicKey && publicKey.length === 32, 'Expected valid publicKey, 32-bytes');
   assert(secretKey && secretKey.length === 64, 'Expected valid secretKey, 64-bytes');
 
-  return sr25519Sign(publicKey as Uint8Array, secretKey as Uint8Array, message);
+  const messageU8a = u8aToU8a(message);
+
+  return sr25519Sign(publicKey as Uint8Array, secretKey as Uint8Array, messageU8a);
 }

packages/util-crypto/src/schnorrkel/verify.ts
@@ -4,12 +4,17 @@
 
 import '../polyfill';
 
+import { u8aToU8a } from '@polkadot/util';
 import { sr25519Verify } from '@polkadot/wasm-crypto';
 
 /**
  * @name schnorrkelVerify
  * @description Verifies the signature of `message`, using the supplied pair
  */
-export default function schnorrkelVerify (message: Uint8Array, signature: Uint8Array, publicKey: Uint8Array): boolean {
-  return sr25519Verify(signature, message, publicKey);
+export default function schnorrkelVerify (message: Uint8Array | string, signature: Uint8Array | string, publicKey: Uint8Array | string): boolean {
+  const messageU8a = u8aToU8a(message);
+  const signatureU8a = u8aToU8a(signature);
+  const publicKeyU8a = u8aToU8a(publicKey);
+
+  return sr25519Verify(signatureU8a, messageU8a, publicKeyU8a);
 }

plugblockchain/common.js

packages/util-crypto/src/nacl/sign.ts
@@ -5,7 +5,7 @@
 import { Keypair } from '../types';
 
 import nacl from 'tweetnacl';
-import { assert } from '@plugnet/util';
+import { assert, u8aToU8a } from '@plugnet/util';
 import { isReady, ed25519Sign } from '@plugnet/wasm-crypto';
 
 /**
@@ -22,10 +22,12 @@ import { isReady, ed25519Sign } from '@plugnet/wasm-crypto';
  * naclSign([...], [...]); // => [...]
  * ```
  */
-export default function naclSign (message: Uint8Array, { publicKey, secretKey }: Partial<Keypair>): Uint8Array {
+export default function naclSign (message: Uint8Array | string, { publicKey, secretKey }: Partial<Keypair>): Uint8Array {
   assert(secretKey, 'Expected valid secretKey');
 
+  const messageU8a = u8aToU8a(message);
+
   return isReady()
-    ? ed25519Sign(publicKey as Uint8Array, (secretKey as Uint8Array).subarray(0, 32), message)
-    : nacl.sign.detached(message, secretKey as Uint8Array);
+    ? ed25519Sign(publicKey as Uint8Array, (secretKey as Uint8Array).subarray(0, 32), messageU8a)
+    : nacl.sign.detached(messageU8a, secretKey as Uint8Array);
 }

packages/util-crypto/src/nacl/verify.ts
@@ -3,6 +3,7 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import nacl from 'tweetnacl';
+import { u8aToU8a } from '@plugnet/util';
 import { isReady, ed25519Verify } from '@plugnet/wasm-crypto';
 
 /**
@@ -19,8 +20,12 @@ import { isReady, ed25519Verify } from '@plugnet/wasm-crypto';
  * naclVerify([...], [...], [...]); // => true/false
  * ```
  */
-export default function naclVerify (message: Uint8Array, signature: Uint8Array, publicKey: Uint8Array): boolean {
+export default function naclVerify (message: Uint8Array | string, signature: Uint8Array | string, publicKey: Uint8Array | string): boolean {
+  const messageU8a = u8aToU8a(message);
+  const signatureU8a = u8aToU8a(signature);
+  const publicKeyU8a = u8aToU8a(publicKey);
+
   return isReady()
-    ? ed25519Verify(signature, message, publicKey)
-    : nacl.sign.detached.verify(message, signature, publicKey);
+    ? ed25519Verify(signatureU8a, messageU8a, publicKeyU8a)
+    : nacl.sign.detached.verify(messageU8a, signatureU8a, publicKeyU8a);
 }

packages/util-crypto/src/schnorrkel/sign.ts
@@ -6,16 +6,18 @@ import { Keypair } from '../types';
 
 import '../polyfill';
 
-import { assert } from '@plugnet/util';
+import { assert, u8aToU8a } from '@plugnet/util';
 import { sr25519Sign } from '@plugnet/wasm-crypto';
 
 /**
  * @name schnorrkelSign
  * @description Returns message signature of `message`, using the supplied pair
  */
-export default function schnorrkelSign (message: Uint8Array, { publicKey, secretKey }: Partial<Keypair>): Uint8Array {
+export default function schnorrkelSign (message: Uint8Array | string, { publicKey, secretKey }: Partial<Keypair>): Uint8Array {
   assert(publicKey && publicKey.length === 32, 'Expected valid publicKey, 32-bytes');
   assert(secretKey && secretKey.length === 64, 'Expected valid secretKey, 64-bytes');
 
-  return sr25519Sign(publicKey as Uint8Array, secretKey as Uint8Array, message);
+  const messageU8a = u8aToU8a(message);
+
+  return sr25519Sign(publicKey as Uint8Array, secretKey as Uint8Array, messageU8a);
 }

packages/util-crypto/src/schnorrkel/verify.ts
@@ -4,12 +4,17 @@
 
 import '../polyfill';
 
+import { u8aToU8a } from '@plugnet/util';
 import { sr25519Verify } from '@plugnet/wasm-crypto';
 
 /**
  * @name schnorrkelVerify
  * @description Verifies the signature of `message`, using the supplied pair
  */
-export default function schnorrkelVerify (message: Uint8Array, signature: Uint8Array, publicKey: Uint8Array): boolean {
-  return sr25519Verify(signature, message, publicKey);
+export default function schnorrkelVerify (message: Uint8Array | string, signature: Uint8Array | string, publicKey: Uint8Array | string): boolean {
+  const messageU8a = u8aToU8a(message);
+  const signatureU8a = u8aToU8a(signature);
+  const publicKeyU8a = u8aToU8a(publicKey);
+
+  return sr25519Verify(signatureU8a, messageU8a, publicKeyU8a);
 }