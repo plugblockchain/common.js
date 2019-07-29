polkadot-js/common

Swap to using eslint (#435)
* WIP

* ... and more

* yet more...

* and examples (more-or-less) fixed

* Only problematic promisify left...

* Last batch

* rm config header

* Update

* Use Record

* Bumps

.eslintrc.js
@@ -1,0 +1,1 @@
+module.exports = require('@polkadot/dev/config/eslint');

plugblockchain/common.js

.eslintrc.js
@@ -1,0 +1,1 @@
+module.exports = require('@plugnet/dev/config/eslint');